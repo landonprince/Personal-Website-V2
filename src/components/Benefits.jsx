a6d841f51f5a6df0d307d5a23cfaea66390cb636
landonprince <landoncprince@gmail.com>
2024-07-08 16:29:25 -0500

    v9

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -20,9 +20,9 @@ const Benefits = () => {
           {benefits.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] group"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
+              // style={{
+              //   backgroundImage: `url(${item.backgroundUrl})`,
+              // }}
               key={item.id}
             >
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">

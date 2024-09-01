bd701e23c6718a1214bf35f7c22471da4abc8fc3
landonprince <landoncprince@gmail.com>
2024-09-01 16:17:04 -0500

    v26

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -49,7 +49,7 @@ const Services = () => {
           <div className="relative z-1 flex xl:items-start items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-14 xl:h-[46rem]">
             <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
               <img
-                className="w-full h-full object-cover md:object-right"
+                className="w-full h-full object-cover object-right"
                 width={800}
                 alt="Smartest AI"
                 height={730}
